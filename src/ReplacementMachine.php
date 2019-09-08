288490519dcd01774e06d72ecad452ac44db2ad9
Michael Holloway <mholloway@wikimedia.org>
2019-09-08 13:24:56 -0400

    Finish port of ReplacementMachine
    Finishes the port of the ReplacementMachine class. Unfortunately this
    means that it can no longer be tested solely within the context of this
    library, since conversion depends on DOM traversal and manipulation
    utilities in the main Parsoid repo. It will be imported and tested
    there.
    
    The Language tests will be moved into the main Parsoid repo, since they
    will have additional Parsoid dependencies.
    
    A new ZhReplacementMachine class used in Parsoid\Language\LanguageZh is
    introduced here since the parent class lives here (and because the
    porting commit there is quite large).
    
    Change-Id: Ie61a08c200d07f6977fafa7b6bbe802b060fa713

--- src/ReplacementMachine.php
+++ src/ReplacementMachine.php
@@ -2,7 +2,11 @@
 
 namespace Wikimedia\LangConv;
 
-/* Partial port of ReplacementMachine.js for testing. */
+use DOMDocument;
+use DOMDocumentFragment;
+use DOMNode;
+use Wikimedia\Assert\Assert;
+
 class ReplacementMachine {
 
 	private $baseLanguage;
@@ -12,7 +16,7 @@ class ReplacementMachine {
 	/**
 	 * ReplacementMachine constructor.
 	 * @param string $baseLanguage
-	 * @param array $codes
+	 * @param string[] $codes
 	 */
 	public function __construct( $baseLanguage, $codes ) {
 		$this->baseLanguage = $baseLanguage;
@@ -20,17 +24,22 @@ class ReplacementMachine {
 		foreach ( $codes as $code ) {
 			$bracketMachines = [];
 			foreach ( $codes as $code2 ) {
-				if ( !$this->isValidLanguagePair( $code, $code2 ) ) {
+				if ( !$this->validCodePair( $code, $code2 ) ) {
 					continue;
 				}
-				$dstCode = $code === $code2 ? "noop" : $code2;
+				$dstCode = $code === $code2 ? 'noop' : $code2;
 				$bracketMachines[$code2] = $this->loadFST( "brack-$code-$dstCode", true );
 			}
 			$this->machines[$code] = [
-				"convert" => $this->loadFST( "trans-$code" ),
-				"bracket" => $bracketMachines,
+				'convert' => $this->loadFST( "trans-$code" ),
+				'bracket' => $bracketMachines,
 			];
 		}
+	}
+
+	/** @return string[] */
+	public function getCodes() {
+		return $this->codes;
 	}
 
 	/**
@@ -44,70 +53,159 @@ class ReplacementMachine {
 	}
 
 	/**
+	 * Override this method in subclass if you want to limit the possible code pairs bracketed.
+	 * (For example, zh has a large number of variants, but we typically want to use only a limited
+	 * number of these as possible invert codes.)
+	 * @param string $destCode
+	 * @param string $invertCode
+	 * @return bool whether this is a valid bracketing pair.
+	 */
+	public function validCodePair( $destCode, $invertCode ) {
+		return true;
+	}
+
+	/**
+	 * Quantify a guess about the "native" language of string `s`.
+	 * We will be converting *to* `destCode`, and our guess is that when we round trip we'll want
+	 * to convert back to `invertCode` (so `invertCode` is our guess about the actual language of
+	 * `s`).
+	 * If we were to make this encoding, the returned value `unsafe` is the number of codepoints
+	 * we'd have to specially-escape, `safe` is the number of codepoints we wouldn't have to
+	 * escape, and `len` is the total number of codepoints in `s`.  Generally lower values of
+	 * `nonsafe` indicate a better guess for `invertCode`.
+	 * @param string $s
+	 * @param string $destCode
+	 * @param string $invertCode
+	 * @return array Statistics about the given guess, containing the keys 'safe', 'unsafe', and
+	 * 'length' (should be `safe + unsafe`.)
+	 */
+	public function countBrackets( $s, $destCode, $invertCode ) {
+		Assert::precondition( $this->validCodePair( $destCode, $invertCode ),
+			"Invalid code pair: $destCode/$invertCode" );
+		$m = $this->machines[$destCode]['bracket'][$invertCode];
+		// call array_values on the result of unpack() to transform from a 1- to 0-indexed array
+		$bytes = array_values( unpack( 'C*', $s ) );
+		$brackets = $m( $bytes, 0, count( $bytes ), true );
+		$safe = 0;
+		$unsafe = 0;
+		for ( $i = 1; $i < count( $brackets ); $i++ ) {
+			$safe += ( $brackets[$i] - $brackets[$i - 1] );
+			if ( ++$i < count( $brackets ) ) {
+				$unsafe += ( $brackets[$i] - $brackets[$i - 1] );
+			}
+		}
+		// Note that this is counting codepoints, not UTF-8 code units.
+		return [
+			'safe' => $safe,
+			'unsafe' => $unsafe,
+			'length' => $brackets[count( $brackets ) - 1]
+		];
+	}
+
+	/**
+	 * Replace the given text Node with converted text, protecting any markup which can't be
+	 * round-tripped back to `invertCode` with appropriate synthetic language-converter markup.
+	 * @param DOMNode $textNode
+	 * @param string $destCode
+	 * @param string $invertCode
+	 * @return DOMNode
+	 */
+	public function replace( $textNode, $destCode, $invertCode ) {
+		$fragment = $this->convert(
+			$textNode->ownerDocument,
+			$textNode->textContent,
+			$destCode,
+			$invertCode
+		);
+		// Was a change made?
+		$next = $textNode->nextSibling;
+		if (
+			// `fragment` has exactly 1 child.
+			$fragment->firstChild && !$fragment->firstChild->nextSibling &&
+			// `fragment.firstChild` is a DOM text node
+			$fragment->firstChild->nodeType === 3 &&
+			// `textNode` is a DOM text node
+			$textNode->nodeType === 3 &&
+			$textNode->textContent === $fragment->textContent
+		) {
+			return $next; // No change.
+		}
+		$textNode->parentNode->replaceChild( $fragment, $textNode );
+		return $next;
+	}
+
+	/**
 	 * Convert a string of text.
-	 * @param string $str text to convert
+	 * @param DOMDocument $document
+	 * @param string $s text to convert
 	 * @param string $destCode destination language code
 	 * @param string $invertCode
-	 * @return string converted text
+	 * @return DOMDocumentFragment DocumentFragment containing converted text
 	 * @suppress PhanTypeInvalidCallableArraySize
 	 */
-	public function convert( $str, $destCode, $invertCode ) {
+	public function convert( $document, $s, $destCode, $invertCode ) {
 		$machine = $this->machines[$destCode];
-		$convertM = $machine["convert"];
-		$bracketM = $machine["bracket"][$invertCode];
+		$convertM = $machine['convert'];
+		$bracketM = $machine['bracket'][$invertCode];
+		$result = $document->createDocumentFragment();
 
 		// call array_values on the result of unpack() to transform from a 1- to 0-indexed array
-		$bytes = array_values( unpack( 'C*', $str ) );
-
+		$bytes = array_values( unpack( 'C*', $s ) );
 		$brackets = $bracketM( $bytes );
-		$result = "";
+
 		for ( $i = 1, $len = count( $brackets ); $i < $len; $i++ ) {
 			// A safe string
 			$safe = $convertM( $bytes, $brackets[$i - 1], $brackets[$i] );
 			if ( strlen( $safe ) > 0 ) {
-				$result .= $safe;
+				$result->appendChild( $document->createTextNode( $safe ) );
 			}
 			if ( ++$i < count( $brackets ) ) {
 				// An unsafe string
-				// $orig = array_slice( $bytes, $brackets[$i-1], $brackets[$i] );
+				$orig = call_user_func_array(
+					'pack',
+					array_merge( [ "C*" ], array_slice( $bytes, $brackets[$i - 1], $brackets[$i] ) )
+				);
 				$unsafe = $convertM( $bytes, $brackets[$i - 1], $brackets[$i] );
-				// DOM markup stuff omitted
+				$span = $document->createElement( 'span' );
+				$span->textContent = $unsafe;
+				$span->setAttribute( 'typeof', 'mw:LanguageVariant' );
+				// If this is an anomalous piece of text in a paragraph otherwise written in
+				// destCode, then it's possible invertCode === destCode. In this case try to pick a
+				// more appropriate invertCode !== destCode.
+				$ic = $invertCode;
+				if ( $ic === $destCode ) {
+					$cs = array_filter( $this->codes, function ( $code ) use ( $destCode ) {
+						return $code !== $destCode;
+					} );
+					$cs = array_map( function ( $code ) use ( $orig ) {
+						return [
+							'code' => $code,
+							'stats' => $this->countBrackets( $orig, $code, $code ),
+						];
+					}, $cs );
+					uasort( $cs, function ( $a, $b ) {
+						return $a['stats']['unsafe'] - $b['stats']['unsafe'];
+					} );
+					if ( count( $cs ) === 0 ) {
+						$ic = '-';
+					} else {
+						$ic = $cs[0]['code'];
+						$span->setAttribute( 'data-mw-variant-lang', $ic );
+					}
+				}
+				$span->setAttribute( 'data-mw-variant', json_encode( [
+					'twoway' => [
+						[ 'l' => $ic, 't' => $orig ],
+						[ 'l' => $destCode, 't' => $unsafe ],
+					],
+					'rt' => true /* Synthetic markup used for round-tripping */
+				] ) );
 				if ( strlen( $unsafe ) > 0 ) {
-					$result .= $unsafe;
+					$result->appendChild( $span );
 				}
 			}
 		}
 		return $result;
-	}
-
-	// TODO: Consolidate Language classes into this repo?
-	private function isValidLanguagePair( $destCode, $invertCode ) {
-		// For now this only seems critical for zh
-		if ( substr( $destCode, 0, 2 ) !== "zh" ) {
-			return true;
-		}
-		switch ( $destCode ) {
-			case "zh-cn":
-				if ( $invertCode === "zh-tw" ) {
-					return true;
-				}
-				// else, fall through
-			case "zh-sg":
-			case "zh-my":
-			case "zh-hans":
-				return $invertCode === "zh-hant";
-			case "zh-tw":
-				if ( $invertCode === "zh-cn" ) {
-					return true;
-				}
-				// else, fall through
-			case "zh-hk":
-			case "zh-mo":
-			case "zh-hant":
-				return $invertCode === "zh-hans";
-			default:
-				return false;
-		}
 	}
 
 }
